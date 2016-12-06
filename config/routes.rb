99368409946d3958a4b7f2c440301d892aec7067
Alex <alexthetford@gmail.com>
2016-12-05 21:00:09 -0500

    added orders controller and views as well as played with product styles

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
