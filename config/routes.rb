d42b3a537d003db6ff6c4bc5e76e65589a37b546
Alex <alexthetford@gmail.com>
2017-01-09 19:42:07 -0500

    finished off stripe payment form with a controller and route to post the form

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
+  post 'payments/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
